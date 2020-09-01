64d1354659b72a386221f22f6d19d5e6fb31db68
Frances Elliott <42477011+fscelliott@users.noreply.github.com>
2020-09-01 12:11:39 -0600

    Update application.py

--- application.py
+++ application.py
@@ -12,13 +12,10 @@ from operator import itemgetter
 #from optimizely_config_manager import OptimizelyConfigManager
 
 
-'''FE changes:
-- I had to update the SDK version to the latest in requirements.txt
-- I guess the older SDK required to you create a config locally (loptimizely_config_manager.py)
-which we don't need any more ... so I had to delete that file and references to it (get_obj)
-...after which, the calls all worked just fine, I guess the backwards compatibility is pretty nice :) 
-so I renamed project_ID to SDK_KEY, and I renamed config_manager to optimizely_client to better reflect the current SDK
-also, is it just me, or is the sorting alg not actually working on the site itself?
+'''2020 updates:
+- updated the SDK version to the latest in requirements.txt
+- deleted optimizely_config_manager.py + its references (get_obj)
+- renamed project_ID to SDK_KEY, renamed config_manager to optimizely_client to better reflect the current SDK
 '''
 from optimizely import optimizely
 from optimizely.config_manager import PollingConfigManager
@@ -34,15 +31,12 @@ CONF_MANAGER = PollingConfigManager(
    update_interval=10
 )
 
-# FE END CHANGES
+
 
 
 from flask import Flask, render_template, request
 
-# update project_id
-#FE:actually this is the per-env SDK key! (you can tell by lookin @ optimizely_config_manager.py, where it looks at <project_id>.json for datafile)
-#FE:not the proejcdt ID which you see in the URL like his:
-#FE:https://app.optimizely.com/v2/projects/18139620475/
+
 
 
 application = Flask(__name__, static_folder='images')
